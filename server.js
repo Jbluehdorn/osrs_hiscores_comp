87c2825e06b2137383bc5b3a8977354a54e4c991
Jordan Bluehdorn <jbluehdorn@alphabytes.io>
2018-09-20 15:27:00 -0700

    Added icons and image route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const path = require('path')
 
 app.use(express.static('dist'))
 
+app.get('/api/images/:name', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/resources/images/${req.params.name}`))
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
